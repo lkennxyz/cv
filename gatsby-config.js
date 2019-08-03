a22128b40366cfea41dbbc89581077b7c0e39f13
Liam Kennedy <liam@lkenn.xyz>
2019-08-03 10:50:19 +0100

    Changed manifest colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ const puppeteer = require('puppeteer');
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000066`,
+        theme_color: `#000066`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
